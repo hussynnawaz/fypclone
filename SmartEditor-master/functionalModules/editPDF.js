b39c908e9a343c89dae39e256124c84350343fac
Hussain Ahmed <92596195+hussynnawaz@users.noreply.github.com>
2022-12-24 20:23:44 +0500

    Add files via upload

---
+++ SmartEditor-master/functionalModules/editPDF.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, Button, View, SafeAreaView } from "react-native";
+
+const Separator = () => <View style={styles.separator} />;
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    {/*<View>
+      
+      <Button
+        title="OPEN PDF"
+       color="#FF2D55"
+      />
+    </View> */}
+    {/* <Separator /> */}
+    <View>
+      {/* This button should upload a file from user in PDF Format only, and then user will be given some templates, option to directly edit text on any section of document*/}
+      <Button title="EDIT PDF" color="#FF2D55" onPress={uploadfile} />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 110,
+  },
+
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: "#F0F0F0",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default App;
